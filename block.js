21f8b52d0d836610eb961c0ecffcd031b9cee45f
UnciaBit <ub.github@unciabit.com>
2021-12-29 20:30:49 +0900

    Improved POW system

--- block.js
+++ block.js
@@ -1,5 +1,7 @@
 const {GENESIS_DATA,MINE_RATE} = require('./config');
 const cryptoHash = require('./cryptoHash');
+const hexToBinary = require('hex-to-binary');
+
 class Block{
     constructor({timestamp, lastHash, hash, data, nonce, difficulty}){
         this.timestamp = timestamp;
@@ -28,7 +30,7 @@ class Block{
                 timestamp
             });
             hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
-        } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
+        } while (hexToBinary(hash).substring(0, difficulty) !== '0'.repeat(difficulty));
 
         return new this({
             timestamp: timestamp,
