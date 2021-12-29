aff93563d7e4721cc52dff659cfcf1595772af1c
UnciaBit <ub.github@unciabit.com>
2021-12-29 20:12:43 +0900

    Added dynamic difficulty

--- block.js
+++ block.js
@@ -1,4 +1,4 @@
-const {GENESIS_DATA} = require('./config');
+const {GENESIS_DATA,MINE_RATE} = require('./config');
 const cryptoHash = require('./cryptoHash');
 class Block{
     constructor({timestamp, lastHash, hash, data, nonce, difficulty}){
@@ -17,12 +17,16 @@ class Block{
         let hash, timestamp;
         // const timestamp = Date.now();
         const lastHash = lastBlock.hash;
-        const {difficulty} = lastBlock;
+        let {difficulty} = lastBlock;
         let nonce = 0;
 
         do{
             nonce++;
             timestamp = Date.now();
+            difficulty = Block.adjustDifficulty({
+                originalBlock: lastBlock,
+                timestamp
+            });
             hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
         } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
 
@@ -35,6 +39,16 @@ class Block{
             hash
             // hash:cryptoHash(timestamp, lastHash, data, nonce, difficulty)
         });
+    }
+    static adjustDifficulty({originalBlock, timestamp}){
+        const {difficulty} = originalBlock;
+        if (difficulty < 1){
+            return 1;
+        }
+        if(timestamp - originalBlock.timestamp > MINE_RATE){
+            return difficulty - 1;
+        }
+        return difficulty + 1;
     }
 }
 
